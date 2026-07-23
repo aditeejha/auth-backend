9d48342fdd7d0097813ab07540482820c22e12e2
Aditee Jha <aditeejha2005@gmail.com>
2026-07-23 21:16:21 +0530

    changes

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const app=express();
 const users=[];
 app.use(express.json());
 
-function generateToken() {
+function generateToken(){
     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
     let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
+    for(let i=0; i<32; i++){
         token += options[Math.floor(Math.random() * options.length)];
     }
     return token;
