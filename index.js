2d648686081e3043f8748a73847c0fe04e6c7a11
Aditee Jha <aditeejha2005@gmail.com>
2026-07-27 18:02:57 +0530

    changes

--- index.js
+++ index.js
@@ -27,24 +27,24 @@ app.post("/signup", (req, res)=>{
 });
 
 
-app.post("/signin", (req, res)=>{
-    const username=req.body.username;
-    const password=req.body.password;
-    const user=users.find(user=>user.username===username && user.password===password);
-    if(user){
-        const token=generateToken();
-        user.token=token;
-        res.send({
-            token
-        })
-        console.log(users);
-    }else{
-        res.status(403).send({
-            message: "Invalid username or password"
-        })
-    }
-    console.log(users);
-});
+// app.post("/signin", (req, res)=>{
+//     const username=req.body.username;
+//     const password=req.body.password;
+//     const user=users.find(user=>user.username===username && user.password===password);
+//     if(user){
+//         const token=generateToken();
+//         user.token=token;
+//         res.send({
+//             token
+//         })
+//         console.log(users);
+//     }else{
+//         res.status(403).send({
+//             message: "Invalid username or password"
+//         })
+//     }
+//     console.log(users);
+// });
 
 app.get("/me", (req, res)=>{
     const token=req.headers.authorization;
