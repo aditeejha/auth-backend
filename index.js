62dd0eb10bb64dea46ab9f487a4152c0bba1d5a0
Aditee Jha <aditeejha2005@gmail.com>
2026-07-27 18:04:03 +0530

    changes

--- index.js
+++ index.js
@@ -46,6 +46,26 @@ app.post("/signup", (req, res)=>{
 //     console.log(users);
 // });
 
+app.post("/signin",(req, res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+    const user=users.find(user=>user.username===username && user.password===password);
+    if(user){
+        const token=jwt.sign({
+            username: user.username
+        }, JWT_SECRET);
+        user.token= token;
+        res.send({
+            token
+        })
+        console.log(users);
+    } else {
+        res.status(403).send({
+            message: "Invalid username or password"
+        })
+    }
+});
+
 app.get("/me", (req, res)=>{
     const token=req.headers.authorization;
     const user=users.find(user=>user.token===token);
