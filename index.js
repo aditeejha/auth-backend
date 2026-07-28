98344d62afd15321206177d25e6f5009b447bfc3
Aditee Jha <aditeejha2005@gmail.com>
2026-07-28 13:55:50 +0530

    changes

--- index.js
+++ index.js
@@ -101,20 +101,11 @@ function auth(req, res, next){
     }
 }
 
-app.get("/me",auth, (req,res)=>{
-    const token=req.headers.authorization;
-    const userDetails=jwt.verify(token, JWT_SECRET);
-    const username= userDetails.username;
-    const user=users.find(user=>user.username===username);
-    if(user){
-        res.send({
-            username: user.username
-        })
-    }else{
-        res.status(401).send({
-            message: "Unauthorized"
-        })
-    }
-})
+app.get("/me", auth, (req, res)=>{
+    const user=req.user;
+    res.send({
+        username: user.username
+    })
+})//  
 
 app.listen(8002);
